3c2b2d3f782fd243f7acb1d925bca0ecbf49d1b1
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 21:31:25 -0300

    feat: configuring the server to listen on port 3000

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,7 @@ server.get('/teams', async(request, response) => {
     response.type("application/json").code(200);
     return [{id: 1, name: 'Ferrari'}];
 });
+
+server.listen({port: 3000}, () => {
+    console.log('server listen on the port 3000')
+})
