da569f133380cfdcceaf6395f116f1e99dc7f250
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 21:28:38 -0300

    feat: configuring server to receive a GET request on '/teams'

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import fastify from "fastify";
 
 const server = fastify({logger: true});
+
+server.get('/teams', async(request, response) => {
+    response.type("application/json").code(200);
+    return [{id: 1, name: 'Ferrari'}];
+});
